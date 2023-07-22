facab1269db0ffff5581c197de882e90ed60db81
Dishant Gangwar <94188265+dishantgangwar01@users.noreply.github.com>
2023-07-22 21:16:05 +0530

    Update addresses.ts

--- tokenapp/const/addresses.ts
+++ tokenapp/const/addresses.ts
@@ -1,5 +1,5 @@
-export const TRANSFER_CONTRACT_ADDRESS = '0xdE730CDbf97c7077f79cffF7925e943c5c30e38b';
-export const CALIM_TOKEN_CONTRACT_ADDRESS = '0x0a291B436f5DAe18505Eb49d6965c3bb985Adc06';
+export const TRANSFER_CONTRACT_ADDRESS = 'YOUR CONTRACT TRANSFER';
+export const CALIM_TOKEN_CONTRACT_ADDRESS = 'YOUR CONTRACT CLAIM TOKEN';
 
 export const HERO_IMAGE_URL = 'ipfs://QmchyzKkBTfP4G42Nt4d2iexLgJnMuz73CKogCQUs2FAaW/seanwatase_isometric_crypto_transferring_from_one_wallet_to_ano-removebg-preview.png';
 export const FEATURES_IMAGE_URL = 'ipfs://QmcTgfb2AXj1TcVj4i9jSgZc8A8LhxHYCXbu5Mngsx7iVT/Coins.png';
